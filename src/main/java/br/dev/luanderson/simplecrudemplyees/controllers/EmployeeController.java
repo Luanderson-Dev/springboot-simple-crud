084000cf1255457bed3c566b7c328125cf412407
Luanderson-Dev <contato@luanderson.dev.br>
2026-03-23 15:00:29 -0400

    feat: add post endpoint for employee creation.

--- src/main/java/br/dev/luanderson/simplecrudemplyees/controllers/EmployeeController.java
+++ src/main/java/br/dev/luanderson/simplecrudemplyees/controllers/EmployeeController.java
@@ -3,6 +3,7 @@ package br.dev.luanderson.simplecrudemplyees.controllers;
 import br.dev.luanderson.simplecrudemplyees.dtos.EmployeeDto;
 import br.dev.luanderson.simplecrudemplyees.services.EmployeeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,5 +17,11 @@ public class EmployeeController {
     public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable Long id) {
         EmployeeDto employeeDto = employeeService.getEmployeeById(id);
         return ResponseEntity.ok(employeeDto);
+    }
+
+    @PostMapping
+    public ResponseEntity<EmployeeDto> createNewEmployee(@RequestBody EmployeeDto employeeDto) {
+        EmployeeDto createdEmployee = employeeService.createNewEmployee(employeeDto);
+        return new ResponseEntity<>(createdEmployee, HttpStatus.CREATED);
     }
 }
