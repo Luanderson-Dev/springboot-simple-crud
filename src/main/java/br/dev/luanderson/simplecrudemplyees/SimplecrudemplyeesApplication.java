1ef8c14b818b1965fd0e3212d6a8efd24a43a0f7
Luanderson-Dev <contato@luanderson.dev.br>
2026-03-23 12:32:28 -0400

    feat: add project setup from spring initializr

---
+++ src/main/java/br/dev/luanderson/simplecrudemplyees/SimplecrudemplyeesApplication.java
@@ -0,0 +1,13 @@
+package br.dev.luanderson.simplecrudemplyees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplecrudemplyeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplecrudemplyeesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/luanderson/simplecrudemplyees/SimplecrudemplyeesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.luanderson.simplecrudemplyees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplecrudemplyeesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
