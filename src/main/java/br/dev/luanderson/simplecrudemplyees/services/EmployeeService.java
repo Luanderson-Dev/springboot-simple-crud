b7708196c3d30da7ccdd2b9b4c10cc3ac8272394
Luanderson-Dev <contato@luanderson.dev.br>
2026-03-23 15:46:09 -0400

    refactor: adjust EmployeeService to use new DTO definitions.

--- src/main/java/br/dev/luanderson/simplecrudemplyees/services/EmployeeService.java
+++ src/main/java/br/dev/luanderson/simplecrudemplyees/services/EmployeeService.java
@@ -1,6 +1,7 @@
 package br.dev.luanderson.simplecrudemplyees.services;
 
-import br.dev.luanderson.simplecrudemplyees.dtos.EmployeeDto;
+import br.dev.luanderson.simplecrudemplyees.dtos.EmployeeRequestDto;
+import br.dev.luanderson.simplecrudemplyees.dtos.EmployeeResponseDto;
 import br.dev.luanderson.simplecrudemplyees.entities.Employee;
 import br.dev.luanderson.simplecrudemplyees.exceptions.ResourceNotFoundException;
 import br.dev.luanderson.simplecrudemplyees.mapper.EmployeeMapper;
@@ -14,19 +15,19 @@ public class EmployeeService {
     private final EmployeeRepository employeeRepository;
     private final EmployeeMapper employeeMapper;
 
-    public EmployeeDto getEmployeeById(Long id) {
+    public EmployeeResponseDto getEmployeeById(Long id) {
         Employee employee = employeeRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Employee not found with id: " + id));
         return employeeMapper.toDto(employee);
     }
 
-    public EmployeeDto createNewEmployee(EmployeeDto employeeDto) {
+    public EmployeeResponseDto createNewEmployee(EmployeeRequestDto employeeDto) {
         Employee newEmployee = employeeMapper.toEntity(employeeDto);
         Employee savedEmployee = employeeRepository.save(newEmployee);
         return employeeMapper.toDto(savedEmployee);
     }
 
-    public EmployeeDto updateEmployee(Long id, EmployeeDto employeeDto) {
+    public EmployeeResponseDto updateEmployee(Long id, EmployeeRequestDto employeeDto) {
         Employee employee = employeeRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Employee not found with id: " + id));
         employeeMapper.updateEmployeeFromDto(employeeDto, employee);
