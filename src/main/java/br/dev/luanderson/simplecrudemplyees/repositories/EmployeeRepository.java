a0d08512a67c0fd8349441b35f35742fc2462e57
Luanderson-Dev <contato@luanderson.dev.br>
2026-03-23 14:47:43 -0400

    feat: add Employee Repository.

---
+++ src/main/java/br/dev/luanderson/simplecrudemplyees/repositories/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package br.dev.luanderson.simplecrudemplyees.repositories;
+
+import br.dev.luanderson.simplecrudemplyees.entities.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
